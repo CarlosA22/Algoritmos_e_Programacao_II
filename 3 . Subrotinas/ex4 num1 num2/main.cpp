bf44293fdbec55b30a5ef690325f69a59d8b1067
CarlosAugusto <7908953+CarlosA22@users.noreply.github.com>
2023-03-09 17:14:01 -0300

    Add files via upload

--- 3 . Subrotinas/ex4 num1 num2/main.cpp
+++ 3 . Subrotinas/ex4 num1 num2/main.cpp
@@ -1,15 +1,28 @@
 #include <iostream>
 #include <locale.h>
 
-float divisor(dv,dd{
+int divisor(int x, int n){
+    int p,q,m;
 
+        q=x/n;
+        p=q*n;
+        m=x-p;
 
-
+    return 0;
 }
 
 
-int main()
-{
-    cout << "Hello world!" << endl;
+
+int main(){
+    setlocale(LC_ALL,"Portuguese");
+    int x, n;
+
+    std::cout<<"Digite dois números inteiros:"<<std::endl;
+    std::cin>>x>>n;
+
+    std::cout<<"O resto é:";
+    divisor(x,n);
+    std::cout<<divisor(x,n);
+
     return 0;
 }
---
+++ 3 . Subrotinas/ex5 numero perfeito/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <locale.h>
+
+int (){
+
+    return ;
+}
+
+
+
+int main(){
+    setlocale(LC_ALL,"Portuguese");
+
+
+
+    return 0;
+}
