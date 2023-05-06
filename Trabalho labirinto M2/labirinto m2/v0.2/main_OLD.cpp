d2578a558b88f28711e0b70e5780c460c27b0346
CarlosAugusto <7908953+CarlosA22@users.noreply.github.com>
2023-05-06 03:02:47 -0300

    Add files via upload

---
+++ Trabalho labirinto M2/labirinto m2/v0.2/main_OLD.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <windows.h>
+#include <conio.h>
+#include "rotinas.h"
+using namespace std;
+
+int main()
+{
+    int m[10][10];
+    int x,y;
+    bool jogando=false;
+    mostrarCursor(FALSE);
+    menu(m, x, y, jogando);
+    
+    return 0;
+}
+// n tentar fazer contador sem struct, é muito longo e cansativo.
